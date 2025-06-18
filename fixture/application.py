985d33f025a980f158b9a403ca43c1875ab3e2c2
klim162 <klim162@yandex.ru>
2025-06-18 09:59:49 +0700

    Gthdst ifub

---
+++ fixture/application.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from fixture.session import SessionHelper
+
+class Application:
+
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome("/Users/a.klimanov/Downloads/chromedriver-mac-arm64/chromedriver")
+        elif browser == "safari":
+            self.wd = webdriver.Safari()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.wd.implicitly_wait(0.5)
+        self.session = SessionHelper(self)
+        self.base_url = base_url
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get(self.base_url)
+
+    def destroy(self):
+        self.wd.quit()
