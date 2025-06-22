404892804be51206d67a4e211fbf2f7ed2c40bcb
klim162 <klim162@yandex.ru>
2025-06-22 10:05:25 +0700

    [soap] edit url

--- fixture/soap.py
+++ fixture/soap.py
@@ -6,9 +6,10 @@ class SoapHelper:
 
     def __init__(self, app):
         self.app = app
+        self.client = Client(app.config['web']['baseURL'] + "/api/soap/mantisconnect.php?wsdl")
 
     def can_login(self, username, password):
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = self.client
         try:
             client.service.mc_login(username, password)
             return True
@@ -16,7 +17,7 @@ class SoapHelper:
             return False
 
     def get_project_list(self):
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = self.client
         list = []
         try:
             projects = client.service.mc_projects_get_user_accessible(
